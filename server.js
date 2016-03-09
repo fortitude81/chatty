bed921fc37a98af5a7fb8c577099f149903a54bb
Justin Bennett <fortitude1981@gmail.com>
2016-03-09 08:58:26 -0600

    up to black diamond

--- server.js
+++ server.js
@@ -1,1 +1,70 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.listen(8090, function() {		//create server, port 8090
+	console.log('Listening on 8090');
+});
+
+var messages = [
+		{
+			  message: 'Hello friends'
+			, author: 'Ryan'
+		}
+		, {
+			  message: 'Hello?'
+			, author: 'Ryan'
+		}
+		, {
+			  message: 'I see how it is.'
+			, author: 'Ryan'
+		}
+	];
+
+app.options('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send();
+});
+
+//GET endpoint that returns a JSON stringified response of your messages array
+app.get('/', function( req, res ) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+});
+
+app.post('/', function( req, res ) {
+
+	messages.push({
+		message: req.body.message,
+  		time: new Date()
+	});
+
+    res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+
+});
 
